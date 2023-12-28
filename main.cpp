ea6b4272cf9a4d8772fd6ca421e91d3d951dc25f
woxane <mohsen.minavand1384@gmail.com>
2023-12-28 15:25:18 +0330

    Menu function prints added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,96 @@
 #include <iostream> 
 
 
+void PrintMenu() {
+	int WIDTH = 35 ;
+	int LENGTH = 9 ; 
 
+
+	for (int Col = 0 ; Col < LENGTH ; Col++) {
+		// Horizontal Line Parts :
+		if (Col == 0 | Col == LENGTH / 2 | Col == LENGTH - 1) {
+			for (int Row = 0 ; Row < WIDTH ; Row++) {
+				if ((WIDTH + 1) / 2 == Row ) { 
+					if (Col == 0 ) {
+						std::cout << "\u2531" ; 
+						continue ; 
+						
+					} else if (Col == LENGTH / 2) {
+						
+						std::cout << "\u2538";
+						continue ; 
+
+					}
+
+				// Corner Parts : 
+				} else if (Row == 0 & Col == 0) { 
+					std::cout << "\u250D" ; 
+					continue ; 
+
+				} else if (Row == 0 & Col == LENGTH - 1) {
+					std::cout << "\u2515" ; 
+					continue ; 
+
+				} else if (Row == WIDTH - 1 & Col == 0) {
+					std::cout << "\u2511" ;
+					continue ; 
+
+				} else if (Row == WIDTH - 1 & Col == LENGTH - 1) {
+					std::cout << "\u251B" ; 
+					continue ; 
+
+				} else if (Row == WIDTH - 1 & Col == LENGTH / 2) {
+					std::cout << "\u2521" ; 
+					continue ;
+
+				} else if (Row == 0 & Col ==  LENGTH / 2) {
+					std::cout << "\u2529" ; 
+					continue ;
+
+				}
+			
+				if (Row )
+				std::cout << "\u2501";
+			}
+
+		}
+
+		if (!(Col == 0 | Col == LENGTH / 2 | Col == LENGTH - 1)) {
+			//Vertical Line Parts : 
+
+			for (int Row = 0 ; Row < WIDTH ; Row++) {
+				if (Row == 0 | Row == (WIDTH + 1 ) / 2 | Row == WIDTH - 1) {
+					if (!(Col > LENGTH / 2 & Row == (WIDTH + 1) / 2)) {
+						std::cout << "\u2503" ; 
+						continue ;
+					}
+		 		} 
+
+				// Menu : 
+				if (Col == 2  & Row < WIDTH / 2) {
+					std::cout << "   " << "1. New Game" <<  "  " ; 
+					Row += 16 ; 
+
+				} else if (Col == 2 & Row > WIDTH / 2) {
+					std::cout << "2. Leaderboard" ;
+					Row += 14 ;
+
+				} else if (Col == 6) {
+					std::cout << "             " << "3. Exit" << "            " ;
+					Row += 32 ;
+
+				}
+
+				std::cout << " " ;
+
+			}
+		}
+
+		std::cout << std::endl ; 
+	}
+
+
+}
 
 int main() {
 
