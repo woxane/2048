a581a154166cac74b54d634feec282dd245bd672
woxane <mohsen.minavand1384@gmail.com>
2024-01-04 18:52:21 +0330

    Bug solved

--- main.cpp
+++ main.cpp
@@ -158,21 +158,23 @@ void RandomBoard(int Board[4][4], int Times = 2) {
     }
 
 
-	for (int time = 0 ; time < Times ; time++) {
-		std::mt19937 Generate(static_cast<std::mt19937::result_type>(
-		std::chrono::high_resolution_clock::now().time_since_epoch().count()
-		));
+	if (size > 0) {
+		for (int time = 0 ; time < Times ; time++) {
+			std::mt19937 Generate(static_cast<std::mt19937::result_type>(
+			std::chrono::high_resolution_clock::now().time_since_epoch().count()
+			));
 
-		std::uniform_int_distribution<int> Index(0 , size - 1);
-		int RandomIndex = Index(Generate) ;
+			std::uniform_int_distribution<int> Index(0 , size - 1);
+			int RandomIndex = Index(Generate) ;
 
-		int* RandomLoc = new int[2];
-		RandomLoc[0] = EmptyLocs[RandomIndex][0];
-		RandomLoc[1] = EmptyLocs[RandomIndex][1];
+			int* RandomLoc = new int[2];
+			RandomLoc[0] = EmptyLocs[RandomIndex][0];
+			RandomLoc[1] = EmptyLocs[RandomIndex][1];
 
-		Board[RandomLoc[0]][RandomLoc[1]] = RandomNumber();
+			Board[RandomLoc[0]][RandomLoc[1]] = RandomNumber();
 
 
+		}
 	}
 
 
@@ -622,6 +624,7 @@ void NewGame() {
 
 	}
 
+	std::cout << std::endl << "You've Lost . " ;
 
 } 
 
